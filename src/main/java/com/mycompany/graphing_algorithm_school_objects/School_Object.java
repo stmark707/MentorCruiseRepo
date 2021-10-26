f894f34271644fbc1a4a13e9e8613d174a87db61
stmark707 <stmark707@gmail.com>
2021-10-26 14:13:53 -0700

    Initial Commit entire project Student Object class initial

--- src/main/java/com/mycompany/graphing_algorithm_school_objects/School_Object.java
+++ src/main/java/com/mycompany/graphing_algorithm_school_objects/School_Object.java
@@ -11,5 +11,41 @@ package com.mycompany.graphing_algorithm_school_objects;
  */
 public class School_Object
 {
+    public String School_name;
+    public String Student_Type;
+    public int Enrollment_Size;
+    public String School_classification;
     
+    public School_Object(String name, String student_type, int enrollment_size, String school_classification)
+    {
+        this.School_name = name;
+        
+        if (student_type == "Adult" || student_type == "adult")
+        {
+            this.Student_Type = "Adult";
+        }
+        else
+        {
+            this.Student_Type = "Child";
+        }
+        
+        if ( enrollment_size <=1000 || enrollment_size >=0)
+        {
+            this.Enrollment_Size = enrollment_size;
+        }
+        else
+        {
+            this.Enrollment_Size = 1;
+        }
+        
+        if (school_classification == "Private" || school_classification == "private")
+        {
+            this.School_classification = school_classification;
+        }
+        else
+        {
+            this.School_classification = "Public";
+        }
+        
+    }
 }
