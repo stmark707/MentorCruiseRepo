f657167b289d9c627ca9e5501d01b86461b70445
stmark707 <stmark707@gmail.com>
2021-10-26 13:59:22 -0700

    Initial Commit entire project Student Object class initial

---
+++ src/main/java/com/mycompany/graphing_algorithm_school_objects/School_Object.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.graphing_algorithm_school_objects;
+
+/**
+ *
+ * @author stmar
+ */
+public class School_Object
+{
+    
+}
