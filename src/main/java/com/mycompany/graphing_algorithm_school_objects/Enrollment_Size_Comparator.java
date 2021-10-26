7f38850bc0cadb5e3f3393f07bc2fd1d7896d709
stmark707 <stmark707@gmail.com>
2021-10-26 16:52:54 -0700

    Object initiation, For loop for unsorted array list and one with a sorted array list based on Enrollment Size

--- src/main/java/com/mycompany/graphing_algorithm_school_objects/Enrollment_Size_Comparator.java
+++ src/main/java/com/mycompany/graphing_algorithm_school_objects/Enrollment_Size_Comparator.java
@@ -12,12 +12,13 @@ import java.util.Comparator;
  * @author stmar
  */
 public class Enrollment_Size_Comparator implements Comparator<School_Object>
-{
+{ 
+    //sorted in descending order
 
     @Override
     public int compare(School_Object School_one, School_Object School_two)
     {
-       return School_two.getSchool_name().compareTo(School_one.getSchool_name());
+       return School_two.getEnrollment_Size()- School_one.getEnrollment_Size();
     }
     
 }
