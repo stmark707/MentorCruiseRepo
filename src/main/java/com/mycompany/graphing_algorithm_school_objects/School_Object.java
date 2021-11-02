012af85f96bd8224f25860ea1950a42739047b1c
Iman Malik <iman0825@vt.edu>
2021-11-02 00:40:36 -0400

    updated the school object file to properly implement comparable in class. This is a better design, and now you can delete the other comparator class.

--- src/main/java/com/mycompany/graphing_algorithm_school_objects/School_Object.java
+++ src/main/java/com/mycompany/graphing_algorithm_school_objects/School_Object.java
@@ -9,118 +9,95 @@ package com.mycompany.graphing_algorithm_school_objects;
  *
  * @author stmar
  */
-public class School_Object
-{
+public class School_Object implements Comparable<School_Object> {
     public String School_name;
     public String Student_Type;
     public int Enrollment_Size;
     public String School_classification;
-    
-    public School_Object(String name, String student_type, int enrollment_size, String school_classification)
-    {
+
+    public School_Object(String name, String student_type, int enrollment_size, String school_classification) {
         this.School_name = name;
-        
-        if (student_type == "Adult" || student_type == "adult")
-        {
+
+        if (student_type == "Adult" || student_type == "adult") {
             this.Student_Type = "Adult";
-        }
-        else
-        {
+        } else {
             this.Student_Type = "Child";
         }
-        
-        if ( enrollment_size <=1000 || enrollment_size >=0)
-        {
+
+        if (enrollment_size <= 1000 || enrollment_size >= 0) {
             this.Enrollment_Size = enrollment_size;
-        }
-        else
-        {
+        } else {
             this.Enrollment_Size = 1;
         }
-        
-        if (school_classification == "Private" || school_classification == "private")
-        {
+
+        if (school_classification == "Private" || school_classification == "private") {
             this.School_classification = school_classification;
-        }
-        else
-        {
+        } else {
             this.School_classification = "Public";
         }
-        
+
     }
 
-    public String getSchool_name()
-    {
+    public String getSchool_name() {
         return School_name;
     }
 
-    public void setSchool_name(String School_name)
-    {
+    public void setSchool_name(String School_name) {
         this.School_name = School_name;
     }
 
-    public String getStudent_Type()
-    {
+    public String getStudent_Type() {
         return Student_Type;
     }
 
-    public void setStudent_Type(String Student_Type)
-    {
-        
-         if (Student_Type == "Adult" ||Student_Type == "adult")
-        {
-             this.Student_Type = Student_Type;
-        }
-        else
-        {
-             this.Student_Type = "Child";
+    public void setStudent_Type(String Student_Type) {
+
+        if (Student_Type == "Adult" || Student_Type == "adult") {
+            this.Student_Type = Student_Type;
+        } else {
+            this.Student_Type = "Child";
         }
     }
 
-    public int getEnrollment_Size()
-    {
+    public int getEnrollment_Size() {
         return Enrollment_Size;
     }
 
-    public void setEnrollment_Size(int Enrollment_Size)
-    {
-        
-         if ( Enrollment_Size <=1000 || Enrollment_Size >=0)
-        {
+    public void setEnrollment_Size(int Enrollment_Size) {
+
+        if (Enrollment_Size <= 1000 || Enrollment_Size >= 0) {
             this.Enrollment_Size = Enrollment_Size;
-        }
-        else
-        {
+        } else {
             this.Enrollment_Size = 1;
         }
     }
 
-    public String getSchool_classification()
-    {
+    public String getSchool_classification() {
         return School_classification;
     }
 
-    public void setSchool_classification(String School_classification)
-    {
+    public void setSchool_classification(String School_classification) {
         this.School_classification = School_classification;
-        if (School_classification == "Private" || School_classification == "private")
-        {
+        if (School_classification == "Private" || School_classification == "private") {
             this.School_classification = School_classification;
-        }
-        else
-        {
+        } else {
             this.School_classification = "Public";
         }
-         }
-        
-        @Override
+    }
+
+    @Override
     public String toString()
-    
-   {                                            
-      return String.format("Below are the following characteristic of the school: %nSchool Name: %s%nSchool Student Type: %s%nSchool Classification type:"
-              + " %s%nSchool Enrollment Size: %d", getSchool_name(), getStudent_Type(), getSchool_classification(), getEnrollment_Size());                                     
-   }
-    
-    
-    
+
+    {
+        return String.format(
+                "Below are the following characteristic of the school: %nSchool Name: %s%nSchool Student Type: %s%nSchool Classification type:"
+                        + " %s%nSchool Enrollment Size: %d",
+                getSchool_name(), getStudent_Type(), getSchool_classification(), getEnrollment_Size());
+    }
+
+    @Override
+    public int compareTo(School_Object School_two) {
+        return School_two.getEnrollment_Size() - this.getEnrollment_Size();
+    }
+
 }
